8e381708a1229f96bcd4e6930ff8acbdb5024779
Temitayo Ogunsusi <ogunsusitemitayo99@gmail.com>
2021-12-28 13:10:29 +0100

    Created phonebook controllers

--- src/controllers/contact.ts
+++ src/controllers/contact.ts
@@ -3,7 +3,13 @@ import * as EmailValidator from 'email-validator';
 import ContactModel from '../model/contact';
 
 const Contact = {
-  create(req: Request, res: Response) {
+  /**
+   *
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} contact object
+   */
+  create(req: Request, res: Response): object {
     const name = (req.body as { name: string }).name;
     const email = (req.body as { email: string }).email;
     const phoneNumber = (req.body as { phoneNumber: string }).phoneNumber;
@@ -22,4 +28,46 @@ const Contact = {
     const contact = new ContactModel().create(user);
     return res.status(201).send(contact);
   },
+
+  /**
+   *
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} contacts array
+   */
+  getAll(req: Request, res: Response): object {
+    const contacts = new ContactModel().findAll();
+    return res.status(200).send(contacts);
+  },
+
+  /**
+   *
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} contact object
+   */
+  getOne(req: Request, res: Response): object {
+    const contact = new ContactModel().findOne((req.params as { id: string }).id);
+    if (!contact) {
+      return res.status(404).send({ message: 'contact not found' });
+    }
+
+    return res.status(200).send(contact);
+  },
+
+  /**
+   *
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} contact object
+   */
+  getOneByPhoneNumber(req: Request, res: Response): object {
+    const contact = new ContactModel().findOneByPhoneNumber((req.params as { phoneNumber: string }).phoneNumber);
+    if (!contact) {
+      return res.status(404).send({ message: 'contact not found' });
+    }
+    return res.status(200).send(contact);
+  },
 };
+
+export default Contact;
